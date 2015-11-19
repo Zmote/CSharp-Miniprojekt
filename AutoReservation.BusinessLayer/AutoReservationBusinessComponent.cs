d2662b169bc0bd32cfa33dc162c0184484d9138a
Zafer <shodaime.hikarikage@gmail.com>
2015-11-19 14:54:04 +0100

    CRUD GetReservations coarsley implemented

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -1,9 +1,24 @@
 ﻿using AutoReservation.Dal;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
+
+        private AutoReservationEntities context;
+
+        public AutoReservationBusinessComponent()
+        {
+            context = new AutoReservationEntities();
+        }
+
+        public IList<Reservation> GetReservations()
+        {
+            var reservationen = from p in context.Reservationen select p;
+            return reservationen.ToList();
+        }
 
         private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
         {
